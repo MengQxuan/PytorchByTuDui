25816bbd9b4072775a34ec9eb69d5b4ff2061a09
没情绪 <3264330095@qq.com>
2026-01-13 14:32:37 +0800

    Transforms的使用（二）

---
+++ P10_dataset_transform.py
@@ -0,0 +1,78 @@
+from PIL import Image
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms
+writer = SummaryWriter("logs")
+img = Image.open("dataset/practice_data/train/ants_image/0013035.jpg")
+print(img)
+
+# totensor的使用
+trans_totensor = transforms.ToTensor()
+img_tensor = trans_totensor(img)
+writer.add_image("ToTensor",img_tensor)
+
+
+# Normalize的使用
+print(img_tensor[0][0][0])
+trans_norm =transforms.Normalize([6,3,2],[9,3,5])
+img_norm = trans_norm(img_tensor)
+# output[channel] = (input[channel] - mean[channel]) / std[channel]
+print(img_norm[0][0][0])
+writer.add_image("Normalize",img_norm)
+
+# Resize的使用
+print(img.size)
+trans_resize = transforms.Resize((512,512))
+# img PIL -> Resize -> img_resize PIL
+img_resize = trans_resize(img)
+# img_resize PIL -> ToTensor -> img_resize Tensor
+img_resize = trans_totensor(img_resize)
+writer.add_image("Resize",img_resize,0)
+print(img_resize)
+
+# Compose - resize - 2
+trans_resize_2=transforms.Resize(512)
+# PIL -> PIL -> Tensor
+trans_compose = transforms.Compose([trans_resize_2, trans_totensor])
+img_resize_2 = trans_compose(img)
+writer.add_image("Resize",img_resize_2,1)
+
+
+# RandomCrop
+trans_random = transforms.RandomCrop(500,1000)
+trans_compose_2 = transforms.Compose([trans_random,trans_totensor])
+for i in range(10):
+    img_crop = trans_compose_2(img)
+    writer.add_image("RandomCrop",img_crop,i)
+
+writer.close()
+
+
+
+# import torchvision
+# from torch.utils.tensorboard import SummaryWriter
+
+# dataset_transform = torchvision.transforms.Compose([
+#     torchvision.transforms.ToTensor()
+# ])
+
+# train_set = torchvision.datasets.CIFAR10(root="./dataset", train=True, transform=dataset_transform, download=True)
+# test_set = torchvision.datasets.CIFAR10(root="./dataset", train=False, transform=dataset_transform, download=True)
+
+# # print(test_set[0])
+# # print(test_set.classes)
+# #
+# # img, target = test_set[0]
+# # print(img)
+# # print(target)
+# # print(test_set.classes[target])
+# # img.show()
+# #
+# # print(test_set[0])
+
+# writer = SummaryWriter("p10")
+# for i in range(10):
+#     img, target = test_set[i]
+#     writer.add_image("test_set", img, i)
+
+# writer.close()
+
--- P9_transforms.py
+++ P9_transforms.py
@@ -48,22 +48,22 @@
 # bees_dataset = MyData(root_dir, image_bees, label_bees, transform=transform)
 
 
-# from PIL import Image
-# from torch.utils.tensorboard import SummaryWriter
-# from torchvision import transforms
-# #python的用法-》tensor数据类型
-# #通过transforms.ToTensor去看两个问题
+from PIL import Image
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms
+#python的用法-》tensor数据类型
+#通过transforms.ToTensor去看两个问题
 
-# #2、为什么我们需要Tensor数据类型
-# img_path ="dataset/practice_data/train/ants_image/0013035.jpg"
-# img = Image.open(img_path)
-# writer = SummaryWriter("logs")
+#2、为什么我们需要Tensor数据类型
+img_path ="dataset/practice_data/train/ants_image/0013035.jpg"
+img = Image.open(img_path)
+writer = SummaryWriter("logs")
 
-# #1、transforms该如何使用(python)
-# tensor_trans = transforms.ToTensor()
-# tensor_img = tensor_trans(img)
+#1、transforms该如何使用(python)
+tensor_trans = transforms.ToTensor()
+tensor_img = tensor_trans(img)
 
-# writer.add_image("Tensor_img",tensor_img)
+writer.add_image("Tensor_img",tensor_img)
 
-# writer.close()
+writer.close()
 
